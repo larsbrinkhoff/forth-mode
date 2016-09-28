9f885433e1927d8b4e64188fef4d178d04fb7ab2
Lars Brinkhoff <lars@nocrew.org>
2016-09-28 09:00:09 +0200

    Use ERT for some light testing as part of build.

--- build.el
+++ build.el
@@ -1,7 +1,22 @@
 (princ (emacs-version))
+
 (let ((generated-autoload-file (concat default-directory "autoloads.el")))
   (update-directory-autoloads "."))
 (load-file "autoloads.el")
 (add-to-list 'load-path ".")
-(let ((result (byte-recompile-directory "." 0)))
-  (kill-emacs (if (string-match "failed" result) 1 0)))
+
+(require 'ert)
+(ert-deftest compile-package ()
+  "Compile package."
+  (should-not (string-match "failed" (byte-recompile-directory "." 0))))
+
+(ert-deftest load-forth-mode ()
+  "Load forth-mode."
+  (should (require 'forth-mode))
+  (should (featurep 'forth-mode))
+  (with-temp-buffer
+    (forth-mode)
+    (should (eq major-mode 'forth-mode))
+    (kill-buffer)))
+
+(ert-run-tests-batch-and-exit)
