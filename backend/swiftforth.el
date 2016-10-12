f418b9aac21d3139144c36414864a25f795df352
Lars Brinkhoff <lars@nocrew.org>
2016-10-12 08:33:17 +0200

    Set process coding system for SwiftForth.

--- backend/swiftforth.el
+++ backend/swiftforth.el
@@ -1,5 +1,12 @@
 (require 'forth-interaction-mode)
 
+(set-process-coding-system (get-buffer-process forth-interaction-buffer)
+			   'raw-text-dos 'raw-text-dos)
+(save-excursion
+  (with-current-buffer forth-interaction-buffer
+    (goto-char (point-max))
+    (insert "\n")))
+
 (forth-interaction-send (concat "include " forth-backend-dir "/swiftforth.fth"))
 
 (provide 'swiftforth)
