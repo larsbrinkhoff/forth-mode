18dce350f46ed09a2bf0dda3f6296f688907512a
Helmut Eller <helmut@msibook>
2016-11-13 21:28:18 +0100

    Include tab (\t) as whitespace delimiter for comments.
    * test/tests.el (forth-backslash-comment-font-lock)
    (forth-paren-comment-font-lock): New tests.
    * forth-mode.el (forth--syntax-propertize): Handle the new test cases.

--- test/tests.el
+++ test/tests.el
@@ -15,3 +15,33 @@
   (should (eq major-mode 'forth-mode))
   (should (and (boundp 'forth-block-mode) forth-block-mode))
   (kill-buffer))
+
+(defmacro forth-with-temp-buffer (contents &rest body)
+  (declare (indent 1) (debug t))
+  `(with-temp-buffer
+     (insert ,contents)
+     (forth-mode)
+     ,@body))
+
+(defun forth-assert-face (content pos face)
+  (forth-with-temp-buffer content
+    (font-lock-fontify-buffer)
+    (should (eq face (get-text-property pos 'face)))))
+
+(ert-deftest forth-paren-comment-font-lock ()
+  (forth-assert-face "( )" 1 font-lock-comment-delimiter-face)
+  (forth-assert-face ".( )" 1 nil)
+  (forth-assert-face "( )" 3 font-lock-comment-delimiter-face)
+  (forth-assert-face " ( )" 2 font-lock-comment-delimiter-face)
+  (forth-assert-face "\t( )" 2 font-lock-comment-delimiter-face)
+  (forth-assert-face "(\t)" 1 font-lock-comment-delimiter-face)
+  (forth-assert-face "(foo) " 3 nil)
+  (forth-assert-face "( foo) " 3 font-lock-comment-face)
+  (forth-assert-face "( a b --
+                        x y )" 1 font-lock-comment-delimiter-face))
+
+(ert-deftest forth-backslash-comment-font-lock ()
+  (forth-assert-face "\\ " 1 font-lock-comment-delimiter-face)
+  (forth-assert-face " \\ " 2 font-lock-comment-delimiter-face)
+  (forth-assert-face "\t\\ " 2 font-lock-comment-delimiter-face)
+  (forth-assert-face "a\\b " 2 nil))
